f08e42f597d23f32b48239b411fcdde192928486
Carson Fuhs <c.fuhs3@gmail.com>
2021-05-22 14:55:10 -0500

    start adding React Bootstrap, and beginning to write some of the cards for the layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Card from './components/Card'
+import WorkoutCard from './components/Card'
+import Title from './components/Title'
+import Card from 'react-bootstrap/Card';
 
 function App() {
+
   return (
     <div className="App">
       
-      <Card />
+      <Title />
+      <WorkoutCard />
 
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,26 @@
 import { Link } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-function Card(props) {
+function WorkoutCard(props) {
   return (
-    <div className="card">
-      <h1>Monday</h1>
-      <Link to="/view">
-        <div>View Workout</div>
-      </Link>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Header as="h5">Monday</Card.Header>
+            <Card.Body>
+              <Card.Title>Workout Title</Card.Title>
+              <Button variant="primary">View Workout</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
-export default Card;
+export default WorkoutCard;

956948c391dc0519ab68cd4f572b2b7197909a2e
ada243 <adewale.j.dada@gmail.com>
2021-06-01 17:02:54 -0400

    adds sign up form

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,11 +5,28 @@ const Signup = (props) => {
         const {gState, setGState} = React.useContext(GlobalCtx)
         const {url} = gState
 
-        // const [form, setForm] = React.useState(blank);
+        const [form, setForm] = React.useState({
+            username: "",
+            password: ""
+        });
+        const handleChange = (event) => {
+            setForm({...form, [event.target.name]: event.target.value})
+        }
 
     return (
         <div>
             <h1>Sign up</h1>
+            <br />
+            <form>
+                <h3>Username</h3>
+                <input type="text" name="username" value={form.username} onChange={handleChange} />
+                <h3>Password</h3>
+                <input type="password" name="password" value={form.password} onChange={handleChange} />
+                <br/>
+                <br/>
+                <input type="submit" name="signup" />
+
+            </form>
         </div>
     )
 }
